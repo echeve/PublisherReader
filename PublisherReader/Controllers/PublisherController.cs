ccef677d38c6114b8070237d7cfabf35cf4afb4b
echeve <echeves@gmail.com>
2023-11-28 13:07:09 +0100

    Delete sample controller and generet a new one with operations of get and post to list and sendmessage.

---
+++ PublisherReader/Controllers/PublisherController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PublisherReader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublisherController : ControllerBase
+    {
+        // GET: api/<PublisherController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<PublisherController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
